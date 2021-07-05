9e04b5e0354a3409d2cc1f9ed4405c16a050853e
Aqris T. Satria <aqrist@gmail.com>
2021-07-05 16:34:37 +0700

    Update : - add whatsapp chat

--- resources/views/pengguna/elemen/static_css.blade.php
+++ resources/views/pengguna/elemen/static_css.blade.php
@@ -1,5 +1,6 @@
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 {{ Html::style('user_assets/fonts/icomoon/style.css') }}
 {{ Html::style('user_assets/css/bootstrap.min.css') }}
 
